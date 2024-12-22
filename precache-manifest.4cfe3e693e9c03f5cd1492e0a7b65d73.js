b2c6b7e2c33311f96d645ce1e80f443ef3716687
echo27713 <echooo@gmail.com>
2024-12-21 19:44:58 -0800

    Updates

---
+++ precache-manifest.4cfe3e693e9c03f5cd1492e0a7b65d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5407b14a02932a2b540f68454ff150af",
+    "url": "/movie2-app/index.html"
+  },
+  {
+    "revision": "ba38568dcdc94b6bfc8f",
+    "url": "/movie2-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "203d4a28aaf785b48fa3",
+    "url": "/movie2-app/static/js/2.16f2d6b9.chunk.js"
+  },
+  {
+    "revision": "ba38568dcdc94b6bfc8f",
+    "url": "/movie2-app/static/js/main.7988e357.chunk.js"
+  },
+  {
+    "revision": "78981474440f7aaaa4d5",
+    "url": "/movie2-app/static/js/runtime~main.6075f27f.js"
+  }
+]);
